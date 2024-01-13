1da39707c40d1324c265db37e669322ff739c5c8
Hecker5556 <96238375+Hecker5556@users.noreply.github.com>
2024-01-13 14:02:56 +0100

    starch hi

--- autocropper.py
+++ autocropper.py
@@ -92,7 +92,7 @@ class autocrop:
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         _,thresh = cv2.threshold(gray,self.threshold,255,cv2.THRESH_BINARY)
         contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        cnt = contours[0]
+        cnt = max(contours, key=cv2.contourArea)
         x,y,w,h = cv2.boundingRect(cnt)
         crop = img[y:y+h,x:x+w]
         cv2.imwrite(f'cropped{filepath}',crop)
